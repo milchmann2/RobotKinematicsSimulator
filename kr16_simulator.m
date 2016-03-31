90ae4737dccfc8a8fc395a5f777e1781d34b6952
milchmann2 <milchmann@gmail.com>
2016-03-31 18:35:55 -0400

    Angle for omega calculation now in rad. Gyros are now in front of joints -> don't move when the joint is actuated.

--- kr16_simulator.m
+++ kr16_simulator.m
@@ -3,13 +3,14 @@ function kr16_simulator()
 %
 clc;
 close all;
+clear all;
 
 % %Parameters for Trajectory Generation
-t_ipo=0.1;
-amax=1500;
-vc=500;
-pose=3;
-clear1=1;
+% t_ipo=0.1;
+% amax=1500;
+% vc=500;
+% pose=3;
+% clear1=1;
  
 robot = Robot();
 %robot.bas(3,4)=100;
@@ -18,7 +19,7 @@ robot = Robot();
 
 %%%%%%%%%%%%%%%%%%%%%
 %*********************
-t_ipo=0.01;
+t_ipo=0.02;
 amax=1500;
 vc=500;
 pose=3;
@@ -83,26 +84,77 @@ robot.Base(3,4)=400;
 % e{2}=[200,20, 100, 0, 0, 0, 0];
 % e{3}=[0,300, 0, 90, 0, 0, 0];
 
-e{1}=[930.91,0,1485,0,180,0,0];
-% e{2}=[500,-1000,1000,0,180,0,2*vc];
-% e{2}=[500,-1000,1000,0,180,0,2*vc];
-% e{3}=[500,-1000,400,0,180,0,vc/2];
-% e{4}=[500,-1000,1000,0,180,0,vc];
-% e{5}=[500,-1100,1000,-90,90,0,vc];
-% e{6}=[500,-1500,1000,-90,90,0,vc/2];
-% e{7}=[500,-1100,1000,-90,90,0,vc];
-% e{8}=[500,1000,1000,0,180,0,vc];
-% e{9}=[500,1000,400,0,180,0,vc/2];
-% e{10}=[500,1000,1000,0,180,0,vc];
-% e{11}=[500,1100,1000,90,90,0,vc];
-% e{12}=[500,1500,1000,90,90,0,vc/2];
-% e{13}=[500,1100,1000,90,90,0,vc];
-% e{14}=[930.91,0,1485,0,180,0,2*vc];
+%cc4
+% e{1}=[930.91,0,400,0,180,0,0];
+% e{2}=[930.91,0,1600,0,180,0,2*vc];
 
-%e{1}=[1725,0,1075,0,90,0,0];
-e{1}=[1600,0,1075,0,90,0,0];
-e{2}=[0,1600,1075,0,0,0,90];
-% e{2}=[500,-1000,1000,0,180,0,2*vc];
+%% wide movements
+e{1}=[0, 1400,1800,0,0,-90,0];
+e{2}=[1000,0,100,90,0,180,2*vc];
+e{3}=[100,100,2200,0,0,0,2*vc];
+% e{4}=[700, 700,700,0,0,-90,0];
+% e{5}=[700,-700,100,-90,0,-90,2*vc];
+% e{6}=[1400,100,1000,-90,90,0,2*vc];
+
+% e{1}=[800, 1000,400,0,0,0,0];
+%  e{2}=[1000,0,100,0,0,0,2*vc];
+% e{3}=[800, 1400,350,0,0,-90,2*vc];
+% e{4}=[1000,800,900,90,0,180,2*vc];
+
+
+% e{5}=[1000,1000,500,90,0,0,2*vc];
+% e{6}=[1000,500,1000,0,90,0,2*vc];
+% e{7}=[1000,1000,500,0,0,0,2*vc];
+% e{8}=[1000,500,1000,90,0,0,2*vc];
+% 
+% e{9}=[1000,1000,500,90,0,0,2*vc];
+% e{10}=[500,1000,1000,90,0,90,2*vc];
+% e{11}=[1000,1000,500,90,0,0,2*vc];
+% e{12}=[500,1000,1000,0,90,0,2*vc];
+% 
+% e{13}=[1000,1000,500,10,10,10,2*vc];
+% e{14}=[1000,1000,510,20,20,20,2*vc];
+% e{15}=[1000,1000,520,30,30,30,2*vc];
+% e{16}=[1000,1000,530,40,40,40,2*vc];
+% 
+% e{14}=[0, 1400,1800,0,0,-90,0];
+% e{15}=[1000,0,100,90,0,180,2*vc];
+% e{16}=[100,100,2200,0,0,0,2*vc];
+% e{17}=[700, 700,700,0,0,-90,0];
+% e{18}=[700,-700,100,-90,0,-90,2*vc];
+% e{19}=[1400,100,1000,-90,90,0,2*vc];
+% 
+% 
+% e{20}=[1000,1000,510,20,20,20,2*vc];
+% e{21}=[700,-700,100,-90,0,-90,2*vc];
+% e{22}=[1000,0,1000,90,0,180,2*vc];
+% e{23}=[1400,100,1000,-90,90,0,2*vc];
+% e{24}=[0, 1400,1000,0,0,-90,0];
+% e{25}=[1000,500,1000,90,0,0,2*vc];
+% e{26}=[500,1000,1000,0,90,0,2*vc];
+
+% 
+% e{1}=[0, 1400,1000,90,90,90,0];
+% e{2}=[0, 1300,1100,180,90,90,0];
+% e{3}=[0, 1400,1100,180,90,90,0];
+% e{4}=[0, 1300,1100,180,180,180,0];
+% e{5}=[0, 1400,1000,-45,180,-90,0];
+% e{6}=[0, 1300,1100,-90,75,-100,0];
+% e{7}=[0, 1400,1000,-110,130,-180,0];
+% e{8}=[0, 1300,1100,180,-165,-75,0];
+% e{9}=[0, 1400,1000,90,-180,-10,0];
+% e{10}=[0, 1300,1000,0,0,0,0];
+% e{11}=[0, 1400,1000,45,0,0,0];
+% e{12}=[0, 1400,1000,90,0,0,0];
+% e{13}=[0, 1400,1000,146,0,0,0];
+% 
+% e{1}=[0, 1400,1000,0,0,0,0];
+% e{2}=[100, 1300,1100,0,0,0,0];
+% e{3}=[0, 1400,1000,0,0,0,0];
+% e{4}=[100, 1300,1100,0,0,0,0];
+% e{5}=[0, 1400,1000,0,0,0,0];
+% e{6}=[100, 1300,1100,0,0,0,0];
+
 %**************************************************************************
 %Implement your code to calculate the trajectory in axis coordinates here
 %Use create_lin_seg-list, create_lin_int_vec and create_lin_path
@@ -151,73 +203,121 @@ for seg = 2:length(e)
 
         % calculate the forward kinematics to get the translation matrices
         % and thus the positions
-        [T Tsub rot] = robot.fk_craig(q, robot);
-        robot.DetermineLocalJointPositions(Tsub);
+%         [T Tsub rot] = robot.fk_craig(q, robot);
+%         robot.DetermineLocalJointPositions(Tsub);
         % calculate joint rotation and translation matrices
-        [rot1, tr] = gyro.CalculateRotations(q, robot.Joints.PositionsX, robot.Joints.PositionsY, robot.Joints.PositionsZ, robot);
+%         [rot1, tr] = gyro.CalculateRotations(q, robot.Joints.PositionsX, robot.Joints.PositionsY, robot.Joints.PositionsZ, robot);
  
         
         if (q_i == 1) % first position of trajectory segment
+            [T Tsub rot] = robot.fk_craig(q, robot);
             for i=1:joints
-                p{q_i, i} = gyro.CalculatePosition(Tsub{i});
+                if i == 1
+                    p{q_i, i} = gyro.CalculatePosition([1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]);
+                else
+                p{q_i, i} = gyro.CalculatePosition(Tsub{i-1});
+                end
             end
             position(1,:) = deg2rad(q)';
             q1(1) = 0;
+            q2(1) = 0;
+            q3(1) = 0;
         else
             % angle change since the previous time step
             q_d = q(:,q_i) - q(:,q_i-1);
+%             q_d = [0.1; 0.2; 0.3; 0.4; 0.5; 0.6];
+            
             time(q_i) = time(q_i-1) + t_ipo;
             position(q_i,:) = deg2rad(q(:,q_i))';
-            % cc2 need to only send recent q in, not whole matrix
+
             [T Tsub, rot] = robot.fk_craig(q, robot);
+            for i=1:5
+                Tsub{i}(1:3,:) =Tsub{i+1}(1:3,:);
+            end
             for i=1:joints
-                p{q_i, i} = gyro.CalculatePosition(Tsub{i});
+                if i == 1
+                    p{q_i, i} = gyro.CalculatePosition([1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]);
+                else
+                p{q_i, i} = gyro.CalculatePosition(Tsub{i-1});
+                end
+            end
+            if q_i > 1
+
+                    aaaa = 1;
+                    Tsub{6};
+                
             end
             jointVelocities(q_i,:) = robot.CalculateJointVelocities(q_d, t_ipo);
             q1(q_i) = q_d(1)/t_ipo;
+            q2(q_i) = (q_d(1)+q_d(2))/t_ipo;
+            q3(q_i) = (q_d(1)+q_d(2)+q_d(3))/t_ipo;
         end
 
         if (q_i ~= 1)
             %**************************************************************
             % JOINT 1
             for i=1:joints
-                cA = (p{q_i-1, i}{1}+p{q_i-1, i}{2}+p{q_i-1, i}{3})/3;
-                cB  = (p{q_i, i}{1}+p{q_i, i}{2}+p{q_i, i}{3})/3;            
-                H = ((p{q_i-1, i}{1}-cA)*(p{q_i, i}{1}-cB)') + ((p{q_i-1, i}{2}-cA)*(p{q_i, i}{2}-cB)') + ((p{q_i-1, i}{2}-cA)*(p{q_i, i}{2}-cB)');
-                [U S V] = svd(H);
-                R = V*U';
-                if det(R)<0
-                    R(:,2) = R(:,2) * -1;
+                for j=1:3   %x,y,z
+                    cA = (p{q_i-1, i}{j}{1}+p{q_i-1, i}{j}{2}+p{q_i-1, i}{j}{3})/3;
+                    cB  = (p{q_i, i}{j}{1}+p{q_i, i}{j}{2}+p{q_i, i}{j}{3})/3;            
+                    H = ((p{q_i-1, i}{j}{1}-cA)*(p{q_i, i}{j}{1}-cB)') + ((p{q_i-1, i}{j}{2}-cA)*(p{q_i, i}{j}{2}-cB)') + ((p{q_i-1, i}{j}{2}-cA)*(p{q_i, i}{j}{2}-cB)');
+                    [U S V] = svd(H);
+                    R = V*U';
+                    if det(R)<0
+                        R(:,2) = R(:,2) * -1;
+                    end
+
+                    rt = t_2_xyzabc(R, 1);
+                    angle = rt(3 - (j-1)); % gets 3 = x, 2 = y, 1 = z
+                    if abs(angle) < 0.0001
+                        angle = 0;
+                    end
+                    % cc4
+                    if q_i > 1
+                        aaaa = 1;
+                        if i == 6 && j == 1
+                            aaaa = 1;
+                        end
+                    end
+                    
+                    if abs(angle)  > 45
+                        angle = 0;
+                    end
+%                     
+%                     a = rt(1);
+%                     b = rt(2);
+%                     c = rt(3);
+                    RD{3} = [R(1,2) -R(1,1) 0 0; R(1,1) R(1,2) 0 0; 0 0 0 0; 0 0 0 0];
+                    RD{2} = [R(3,1) 0 R(1,1) 0; 0 0 0 0; -R(1,1) 0 R(3,1) 0; 0 0 0 0];
+                    RD{1} = [0 0 0 0; 0 R(2,3) -R(2,2) 0; 0 R(2,2) R(2,3) 0; 0 0 0 0];
+                    if j == 1
+                        R = rotx(angle);
+                    elseif j == 2
+                        R = roty(angle);
+                    elseif j == 3
+                        R = rotz(angle);
+                    end
+                    angle = deg2rad(angle);
+                    omega{j} = (RD{j}*R')*angle/(1/t_ipo);
+                    
+                    if j == 1
+                        gyros{q_i}{i}{j} = omega{j}(3,2);
+                        gyrosX(q_i, i) = omega{j}(3,2);
+                    elseif j == 2
+                        gyros{q_i}{i}{j} = omega{j}(1,3);
+                        gyrosY(q_i, i) = omega{j}(1,3);
+                    elseif j == 3
+                        gyros{q_i}{i}{j} = omega{j}(2,1);
+                        gyrosZ(q_i, i) = omega{j}(2,1);
+                    end
                 end
-    
-                rt = t_2_xyzabc(R, 1);
-                a = rt(1);
-                b = rt(2);
-                c = rt(3);
-                RDz = [R(1,2) -R(1,1) 0 0; R(1,1) R(1,2) 0 0; 0 0 0 0; 0 0 0 0];
-                RDy = [R(3,1) 0 R(1,1) 0; 0 0 0 0; -R(1,1) 0 R(3,1) 0; 0 0 0 0];
-                RDx = [0 0 0 0; 0 R(2,3) -R(2,2) 0; 0 R(2,2) R(2,3) 0; 0 0 0 0];
-                Rx = rotx(c);
-                Ry = roty(b);
-                Rz = rotz(a);
-                % cc1
-                omegaX = (RDx*Rx')*c/(1/t_ipo);
-                omegaY = (RDy*Ry')*b/(1/t_ipo);
-                omegaZ = (RDz*Rz')*a/(1/t_ipo);
-                if i == 4
-                    a = 1;
-                    q_d;
-%                     qg = qg+q_d
-                end
-                
-                gyrosX(q_i, i) = omegaX(3,2);
-                gyrosY(q_i, i) = omegaY(1,3);
-                gyrosZ(q_i, i) = omegaZ(2,1);
             end
  
         else
             for i=1:joints
-                gyrosZ(i, 1) = 0;
+                for j=1:joints
+                    gyros{q_i}{i}{j} = 0;
+                end
             end
         end
         q_i = q_i+1;
@@ -248,7 +348,9 @@ grid on;
 xlabel('X'); ylabel('Y'); zlabel('Z');
 
 % cc2
-%draw_robot_path(q,t_ipo,robot,100,clear1, p);
+draw_robot_path(q,t_ipo,robot,100,clear1, p);
+
+
 % figure(3)
 % axis([0 1700 0 1700]);
 % for j=i:size(p,1)
@@ -278,13 +380,13 @@ for i=1:3
     subplot(3,1,i)
     hold on;
     for j=1:joints
-        if j == 4
-            plot(gyros{i}(:,j));    
-        elseif j == 3
-            plot(gyros{i}(:,j), 'o');
-        else
+%         if j == 4
+%             plot(gyros{i}(:,j));    
+%         elseif j == 3
+%             plot(gyros{i}(:,j), 'o');
+%         else
             plot(gyros{i}(:,j));
-        end
+%         end
     end
     legend(legendStrings);
     title(gyrosTitle{i});
@@ -292,88 +394,102 @@ end
 
 
 
-% log.gyroX = gyrosX;
-% log.gyroY = gyrosY;
-% log.gyroZ = gyrosZ;
-% log.velocity = jointVelocities;
+% log.gyroX = [gyrosX; gyrosX; gyrosX; gyrosX; gyrosX; gyrosX];
+% log.gyroY = [gyrosY; gyrosY; gyrosY; gyrosY; gyrosY; gyrosY];
+% log.gyroZ = [gyrosZ; gyrosZ; gyrosZ; gyrosZ; gyrosZ; gyrosZ];
+% jointVelocities = [jointVelocities;jointVelocities;jointVelocities;jointVelocities;jointVelocities;jointVelocities];
+% log.velocity = jointVelocities./100;
 % numModules = joints;
-% log.time = time;
-% log.position = position;
-% % Identify the base module based on the one that moved least
-% % The index of the smallest value in meanVels corresponds to the base
-% % module, becuase the IMU is before anything that moves.
-% meanVels = mean( [ mean(abs(log.gyroX));
-%                    mean(abs(log.gyroY));
-%                    mean(abs(log.gyroZ)) ] );
-% 
-% for proximal = 1:numModules
-%     for distal = 1:numModules
-% 
-%         proximalModule = proximal; 
-%         distalModule = distal;
-%         a = log.gyroX(:,proximalModule);
-%         b = log.gyroY(:,proximalModule);
-%         c = log.gyroZ(:,proximalModule);
-%         d = log.velocity(:,proximalModule);
-%         proximalVels = [ log.gyroX(:,proximalModule) ...
-%                      log.gyroY(:,proximalModule) ...
-%                      log.gyroZ(:,proximalModule) + log.velocity(:,proximalModule)];
-% 
-%         distalVels = [ log.gyroX(:,distalModule) ...
-%                      log.gyroY(:,distalModule) ...
-%                      log.gyroZ(:,distalModule) ];
-% 
-%         % Rotate baseVels by the joint angle
-%         for i=1:length(log.time)
-%             proximalVels(i,:) = ...
-%                 [R_z(-log.position(i,proximalModule)) * proximalVels(i,:)']';
-%         end
-% 
-%         % Dot-product all the velocities
-%         A = distalVels' * proximalVels;
-% 
-%         % Take the SVD
-%         [U,S,V] = svd(A);
+% log.time = [time;time;time;time;time;time];
+% log.position = [position;position;position;position;position;position];
+
+% cc5
+log.gyroX = gyrosX ;
+log.gyroY = gyrosY;
+log.gyroZ = gyrosZ;
+log.velocity = jointVelocities/10;
+numModules = joints;
+log.time = time;
+log.position = position;
+
+% Identify the base module based on the one that moved least
+% The index of the smallest value in meanVels corresponds to the base
+% module, becuase the IMU is before anything that moves.
+
+% cc5
+meanVels = mean( [ mean(abs(log.gyroX));
+                   mean(abs(log.gyroY));
+                   mean(abs(log.gyroZ)) ] );
+
+wgx = mean(abs(log.gyroX))
+wgy = mean(abs(log.gyroY))
+wgz = mean(abs(log.gyroZ))
+wvel = mean(abs(log.velocity))
+
+for proximal = 1:numModules
+    for distal = 1:numModules
+
+        proximalModule = proximal; 
+        distalModule = distal;
+        proximalVels = [ log.gyroX(:,proximalModule) ...
+                     log.gyroY(:,proximalModule) ...
+                     log.gyroZ(:,proximalModule) + log.velocity(:,proximalModule)];
+
+        distalVels = [ log.gyroX(:,distalModule) ...
+                     log.gyroY(:,distalModule) ...
+                     log.gyroZ(:,distalModule) ];
+
+        % Rotate baseVels by the joint angle
+        for i=1:length(log.time)
+            proximalVels(i,:) = ...
+                [R_z(-log.position(i,proximalModule)) * proximalVels(i,:)']';
+        end
+
+        % Dot-product all the velocities
+        A = distalVels' * proximalVels;
+
+        % Take the SVD
+        [U,S,V] = svd(A);
+        
+        % Get the determinant of U*V
+        S_SO3 = diag( [1, 1, sign(det(U*V))] );
+
+        % Get the rotation
+        R = V * S_SO3 * U';
+
+
+        %% RESIDUAL ERROR
+        errors(proximal,distal) = ...
+            mean(mean((proximalVels - [R*distalVels']').^2));
+        
+        R_tests(:,:,proximal,distal) = R;
+        
+%         %% PLOTTING
+%         figure(123);
+%         plot(log.time,baseVels);
+%         hold on;
+%         plot(log.time,[R*nextVels']','--');
+%         hold off;
 %         
-%         % Get the determinant of U*V
-%         S_SO3 = diag( [1, 1, sign(det(U*V))] );
-% 
-%         % Get the rotation
-%         R = V * S_SO3 * U';
-% 
-% 
-%         %% RESIDUAL ERROR
-%         errors(proximal,distal) = ...
-%             mean(mean((proximalVels - [R*distalVels']').^2));
-%         
-%         R_tests(:,:,proximal,distal) = R;
-%         
-% %         %% PLOTTING
-% %         figure(123);
-% %         plot(log.time,baseVels);
-% %         hold on;
-% %         plot(log.time,[R*nextVels']','--');
-% %         hold off;
-% %         
-% %         legend x y z x y z
-% %         title(['Velocities: ' num2str(proximal) '-->' num2str(distal) ]);
-% %         xlabel('time (sec)');
-% %         ylabel('angular velocity (rad/sec)');
-% %         drawnow;
-% %         pause;
-%         
-%     end
-% end
-% 
-% %% FINAL RESULT
-% 
-% errorThresh = .02;
-% connectionMatrix = errors < errorThresh
-% 
-% figure
-% imagesc(errors);
-% title('Velocity Matching Error');
-% xlabel('distal module');
-% ylabel('proximal module');
-% colorbar;
+%         legend x y z x y z
+%         title(['Velocities: ' num2str(proximal) '-->' num2str(distal) ]);
+%         xlabel('time (sec)');
+%         ylabel('angular velocity (rad/sec)');
+%         drawnow;
+%         pause;
+        
+    end
+end
+
+%% FINAL RESULT
+
+errorThresh = .5;
+connectionMatrix = errors < errorThresh
+
+figure
+imagesc(errors);
+title('Velocity Matching Error');
+xlabel('distal module');
+ylabel('proximal module');
+colorbar;
 
